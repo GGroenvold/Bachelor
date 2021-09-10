2418d4537ab3d9dedf800959fa893e905450b816
s184191 <s184191@student.dtu.dk>
2021-09-10 11:48:55 +0200

    Preliminary testing of AES using PyCryptoDome

--- code/test.py
+++ code/test.py
@@ -1,1 +1,13 @@
-print('Hello, world!')
+from Crypto.Cipher import AES
+from Crypto.Util import Padding
+
+key = b'Sixteen byte key'
+plaintext = Padding.pad(b'This', 16)
+
+cipher = AES.new(key, AES.MODE_ECB)
+ciphertext = cipher.encrypt(plaintext)
+
+decrypt_cipher = AES.new(key, AES.MODE_ECB)
+
+print(ciphertext)
+print(Padding.unpad(decrypt_cipher.decrypt(ciphertext), 16))
