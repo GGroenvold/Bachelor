cd1d75e42e6a7490b50dc86b679901a335ee8795
s184191 <s184191@student.dtu.dk>
2021-09-11 09:30:31 +0200

    Add requirements.txt and simple testing for ff3

--- code/ff3.py
+++ code/ff3.py
@@ -1,21 +1,29 @@
-import math
+from math import ceil
+import logging
 from bitstring import BitArray
 from Crypto.Cipher import AES
 from Crypto.Random import get_random_bytes
 
+# Constants
 RADIX = 10
 MIN_LEN, MAX_LEN = 6, 10
 TWEAK_LEN = 56
 
 
 class FF3:
+
     def __init__(self, key, radix=RADIX):
         self.K = key
         self.radix = radix
         self.cipher = AES.new(key, AES.MODE_ECB)
 
     def encrypt(self, tweak, plaintext):
+        """
 
+        :param tweak: 56 bit
+        :param plaintext:
+        :return:
+        """
         if not (len(tweak) == TWEAK_LEN):
             raise ValueError(f"Tweak must be {TWEAK_LEN} bits")
 
@@ -23,11 +31,13 @@ class FF3:
             raise ValueError(f"Plaintext must have length between {MIN_LEN} and {MAX_LEN}")
 
         n = len(plaintext)
-        u = int(math.ceil(n / 2))
+        u = int(ceil(n / 2))
         v = n - u
 
+        # Split numeral string into two parts
         A, B = plaintext[:u], plaintext[u:]
 
+        # Construct a left and right tweak of 32 bits
         tweak_left = BitArray(tweak[:28]) + BitArray('0b0000')
         tweak_right = BitArray(tweak[32:56]) + BitArray(tweak[28:32]) + BitArray('0b0000')
 
@@ -52,7 +62,12 @@ class FF3:
         return A + B
 
     def decrypt(self, tweak, ciphertext):
-
+        """
+        Decrypt ciphertext encrypted with FF3-1 cipher
+        :param tweak: 56 bit
+        :param ciphertext:
+        :return:
+        """
         if not (len(tweak) == TWEAK_LEN):
             raise ValueError(f"Tweak must be {TWEAK_LEN} bits")
 
@@ -60,7 +75,7 @@ class FF3:
             raise ValueError(f"Plaintext must have length between {MIN_LEN} and {MAX_LEN}")
 
         n = len(ciphertext)
-        u = int(math.ceil(n / 2))
+        u = int(ceil(n / 2))
         v = n - u
         A, B = ciphertext[:u], ciphertext[u:]
 
@@ -91,12 +106,15 @@ if __name__ == '__main__':
     tweak = BitArray(get_random_bytes(TWEAK_LEN // 8))
     key = get_random_bytes(16)
 
-    ff3_cypher = FF3(key)
+    print(tweak)
+    print(key)
+
+    ff3_cipher = FF3(key)
 
     X = '1234567'
 
-    ciphertext = ff3_cypher.encrypt(tweak, X)
-    plaintext = ff3_cypher.decrypt(tweak, ciphertext)
+    ciphertext = ff3_cipher.encrypt(tweak, X)
+    plaintext = ff3_cipher.decrypt(tweak, ciphertext)
 
     print(ciphertext)
     print(plaintext)
---
+++ code/tests/test_ff3.py
@@ -0,0 +1,30 @@
+import pytest
+from code.ff3 import FF3
+from bitstring import BitArray
+
+KEY = b'2\\\x9f\xbb\xd9\x91X4[dP\xc90\x14\xa5\xd8'
+TWEAK = BitArray('0xe7e2f5d699b30c')
+PLAINTEXTS = {'123456', '1234567'}
+
+
+@pytest.fixture
+def ff3_cipher():
+    return FF3(KEY)
+
+
+def test_encrypt_plaintext_format(ff3_cipher):
+    with pytest.raises(ValueError):
+        assert ff3_cipher.encrypt(TWEAK, '123')
+        assert ff3_cipher.encrypt(TWEAK, '12345678910')
+
+
+def test_encrypt_tweak_format(ff3_cipher):
+    with pytest.raises(ValueError):
+        assert ff3_cipher.encrypt(BitArray('0b0000'), '123456')
+        assert ff3_cipher.encrypt(BitArray('0xe7e2f5d699b30c10'), '123456')
+
+
+@pytest.mark.parametrize("plaintext", PLAINTEXTS)
+def test_decrypt(ff3_cipher, plaintext):
+    ciphertext = ff3_cipher.encrypt(TWEAK, plaintext)
+    assert ff3_cipher.decrypt(TWEAK, ciphertext) == plaintext
