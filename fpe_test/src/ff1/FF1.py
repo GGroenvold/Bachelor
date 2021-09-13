f7dabe3247c63c6a4ba087b2ab031530e43ca3d9
Tobias Holtoft <tobiasholtoft@gmail.com>
2021-09-13 15:58:37 +0200

    first implementation of ff1

--- fpe_test/src/ff1/FF1.py
+++ fpe_test/src/ff1/FF1.py
@@ -1,34 +1,91 @@
-import math
-import utils
-
-
 # need real radix
 # need real Tweak and t
 # need own ceil func
-# dont understand PRF func
-radix = 10
-T = b'0101010101010101'
-t = 16
-n = b'1000100010001000'
-u = len(n)//2
-v = len(n)-u
-A = n[:u]
-B = n[u:]
-b = math.ceil(math.ceil(v*math.log2(radix))/8)
-d = 4*math.ceil(b/4)+4
-P = b'00000001'+ b'00000010'+b'00000001' + \
-    radix.to_bytes(3,'big') + b'00001010' + \
-    (u % 256).to_bytes(1,'big') + \
-    len(n).to_bytes(3,'big') + t.to_bytes(1,'big')
+import math
+from utils.utils import num_radix, str_radix
+from Crypto.Cipher import AES
+from Crypto.Random import get_random_bytes
 
-for x in range(0,9):
-    Q = T + (0).to_bytes(-t-b-1,'big') + x.to_bytes(1,'big') + num_radix(B)
-    R = 10
-    
+msg = '0123456789'
+T = b'\x00\x10'
+key = get_random_bytes(16)
+cipher = AES.new(key, AES.MODE_ECB)
+def PRF(X):
+    m = int((len(X)*8)/128)
+    Y0 =  b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'
+    for j in range(m):
+        if (j==0) : 
+            Yj = Y0
+        Xj =  X[j*16:(j*16)+16]
+        Yj = cipher.encrypt(bytes(A^B for A,B in zip(Yj,Xj)))
+    return Yj
 
+def encrypt(msg,T,key):
+    radix = 10
+    t = len(T)
+    n = len(msg)
+    u = n//2
+    v = n-u
+    A = msg[:u]
+    B = msg[u:]
+    b = math.ceil(math.ceil(v*math.log2(radix))/8)
+    d = 4*math.ceil(b/4)+4
+    P = b'\x01'+ b'\x02'+b'\x01' + \
+        radix.to_bytes(3,'big') + b'\n' + \
+        (u % 256).to_bytes(1,'big') + \
+        n.to_bytes(4,'big') + t.to_bytes(4,'big')
 
-print(x)
+    for i in range(10):
+        Q = T + (0).to_bytes((-t-b-1) % 16,'big') + (i).to_bytes(1,'big') + num_radix(radix, B).to_bytes(b,'big')
+        R = PRF(P+Q)
+        S = R
+        for j in range(1, int(math.ceil(d/16))):
+            S = S + cipher.encrypt(bytes(A^B for A,B in zip(R,(j).to_bytes(16,'big'))))
+        S = S[:d]
+        y = int.from_bytes(S,'big')
+        if (i%2 == 0): 
+            m=u
+        else:
+            m=v
+        c = (num_radix(radix,A)+y)%radix**m
+        C = str_radix(radix,m,c)
+        A = B
+        B = C
+    return(A+B)
 
-def tonumber(asd):
-    #make bit to integer
-    return 100
+def decrypt (cphTxt, T, key):
+    radix = 10
+    t = len(T)
+    n = len(cphTxt)
+    u = n//2
+    v = n-u
+    A = cphTxt[:u]
+    B = cphTxt[u:]
+    b = math.ceil(math.ceil(v*math.log2(radix))/8)
+    d = 4*math.ceil(b/4)+4
+    P = b'\x01'+ b'\x02'+b'\x01' + \
+        radix.to_bytes(3,'big') + b'\n' + \
+        (u % 256).to_bytes(1,'big') + \
+        n.to_bytes(4,'big') + t.to_bytes(4,'big')
+
+    for i in range(9,-1,-1):
+        Q = T + (0).to_bytes((-t-b-1) % 16,'big') + (i).to_bytes(1,'big') + num_radix(radix, A).to_bytes(b,'big')
+        R = PRF(P+Q)
+        S = R
+        for j in range(1, int(math.ceil(d/16))):
+            S = S + cipher.encrypt(bytes(A^B for A,B in zip(R,(j).to_bytes(16,'big'))))
+        S = S[:d]
+        y = int.from_bytes(S,'big')
+        if (i%2 == 0): 
+            m=u
+        else:
+            m=v
+        c = (num_radix(radix,B)-y)%radix**m
+        C = str_radix(radix,m,c)
+        B = A
+        A = C
+    return(A+B)
+cphTxt = encrypt(msg,T,key)
+
+print(cphTxt)
+print(decrypt(cphTxt,T,key))
